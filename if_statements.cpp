bc2ef6b7964e379032c5d11c84cc10c03e2bbc1a
chengkahohk <46778468+chengkahohk@users.noreply.github.com>
2022-02-20 14:34:48 +0800

    master

--- if_statements.cpp
+++ if_statements.cpp
@@ -3,12 +3,18 @@ using namespace std;
 
 int main()
 {
-    bool isMale = true;
+    bool isMale = false;
+    bool isTall = true;
 
-    if(isMale){
-        cout << "You are a male";
-
+    if(isMale && isTall){
+        cout << "You are a tall male";
+    } else if(isMale && !isTall) {
+        cout << "You are a short male";
+    } else if(!isMale && isTall) {
+        cout << "You are tall but not male";
+    } else {
+        cout << "You are not male and not tall";
     }
-    return 0;
-}
 
+    return 0;
+}   
