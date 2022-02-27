755470a3e23e6a5610b5d7ec25746be72ca9f722
chengkahohk <46778468+chengkahohk@users.noreply.github.com>
2022-02-27 11:27:30 +0800

    master

--- for_loop.cpp
+++ for_loop.cpp
@@ -1,11 +1,27 @@
-#include <iostream>
+// C++ Program to print the elements of a
+// Two-Dimensional array
+#include<iostream>
 using namespace std;
 
 int main()
 {
-    int nums[] = {1, 3, 5, 7,9};
-    for(int i=0; i<5; i++){
-        cout << nums[i] << endl;
-    }
-} // namespace std;
+	// an array with 3 rows and 2 columns.
+	int x[3][2] = {
+                    {0,1}, 
+                    {2,3}, 
+                    {4,5}
+                };
 
+	// output each array element's value
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 2; j++)
+		{
+            cout << "[" << i << "]";
+            cout << "[" << j << "]";
+			cout << x[i][j]<<endl;
+		}
+	}
+
+	return 0;
+}
---
+++ pointers.cpp
@@ -0,0 +1,11 @@
+ #include <iostream>
+
+int main()
+{
+    int age = 19;
+    double gpa = 2.7;
+    std::string name = "Mike";
+
+    std::cout << &age;
+
+}
